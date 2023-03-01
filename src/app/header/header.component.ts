863217cca61965fd561843055a6a6659e2cf27eb
a-sepulveda-r <ariel.sepulvedar@gmail.com>
2023-03-01 17:36:39 -0300

    commit3

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { AuthService } from "../usuarios/auth.service";
 
 @Component({
   selector: "app-header",
@@ -6,4 +7,6 @@ import { Component } from "@angular/core";
 })
 export class HeaderComponent {
   title: string = "App Angular ";
+
+  constructor(public authService: AuthService) {}
 }
--- src/app/usuarios/login.component.ts
+++ src/app/usuarios/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { catchError, tap, throwError } from "rxjs";
 import Swal from "sweetalert2";
+import { AuthService } from "./auth.service";
 import { Usuario } from "./usuario";
 
 @Component({
@@ -10,15 +13,46 @@ export class LoginComponent {
   titulo: string = "Por favor Sign in";
   usuario: Usuario;
 
-  constructor() {
+  constructor(private authService: AuthService, private router: Router) {
     this.usuario = new Usuario();
+  }
+  ngOnInit() {
+    if (this.authService.isAuthenticated()) {
+      Swal.fire(
+        "Login",
+        `${this.authService.usuario.username} ya estas autentificado!`,
+        "info"
+      );
+      this.router.navigate(["/clientes"]);
+    }
   }
 
   login(): void {
     console.log(this.usuario);
     if (this.usuario.username == null || this.usuario.password == null) {
-      Swal.fire("Error login", "Username or password vacías", "error");
+      Swal.fire("Error Login", "Username o password vacías!", "error");
       return;
     }
+
+    this.authService.login(this.usuario).subscribe(
+      (response) => {
+        console.log(response);
+
+        this.authService.guardarUsuario(response.access_token);
+        this.authService.guardarToken(response.access_token);
+        let usuario = this.authService.usuario;
+        this.router.navigate(["/clientes"]);
+        Swal.fire(
+          "Login",
+          `Hola ${usuario.username}, has iniciado sesión con éxito!`,
+          "success"
+        );
+      },
+      (err) => {
+        if (err.status == 400) {
+          Swal.fire("Error Login", "Usuario o clave incorrectas!", "error");
+        }
+      }
+    );
   }
 }
